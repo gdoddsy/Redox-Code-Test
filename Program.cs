b4e7757401415698c1b5e18d73d31642301324e6
Luka Casey <lukacasaletto2000@gmail.com>
2023-06-22 13:03:10 +1000

    "Completed Redox Code Test exercises"

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Redox_Code_Test
 {
@@ -6,8 +7,77 @@ namespace Redox_Code_Test
     {
         static void Main(string[] args)
         {
-            
+            // EXERCISE 1
 
+            // step 1
+            List<int> numbers = new List<int>();
+            for (int i = 1; i <= 100; i++)
+            {
+                numbers.Add(i);
+            }
+
+            // step 2
+            List<int> evenNumbers = numbers.Where(n => n % 2 == 0).ToList();
+
+            foreach (int num in evenNumbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            // step 3
+            List<int> divisibleNumbers = new List<int>();
+
+            foreach (int num in numbers)
+            {
+                if ((num % 3 == 0 || num % 5 == 0) && !(num % 3 == 0 && num % 5 == 0))
+                {
+                    divisibleNumbers.Add(num);
+                }
+            }
+
+            foreach (int num in divisibleNumbers)
+            {
+                Console.WriteLine(num);
+            }
+            
+        }
+    }
+
+    // EXERCISE 2
+
+    // step 1
+    public class Event
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+
+    // step 2
+    public class EventScheduler
+    {
+        private List<Event> events;
+
+        public void ScheduleEvent(Event newEvent)
+        {
+            // step 3
+            if (events.Any(e => e.DateTime == newEvent.DateTime))
+            {
+                Console.WriteLine("Cannot book event, timeslot already booked.");
+                return;
+            }
+            
+            events.Add(newEvent);
+        }
+
+        public void CancelEvent(Event eventToCancel)
+        {
+            events.Remove(eventToCancel);
+        }
+
+        public List<Event> GetUpcomingEvents()
+        {
+            return events;
         }
     }
 }
