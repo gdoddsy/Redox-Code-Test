9877262c26c3e4b3d5721ee72bb3c582e820ce8d
Yiwen Zhou <evenchou3112@gmail.com>
2023-06-22 12:45:36 +1000

    Yvonne Zhou solution

---
+++ Event.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+
+namespace Redox_Code_Test
+{
+    public class Event
+    {
+        public Event(string name, string location, DateTime startTime)
+        {
+            Name = name;
+            Location = location;
+            StartTime = startTime;
+        }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public DateTime StartTime { get; set; }
+    }
+
+    public class EventScheduler
+    {
+        private List<Event> evtList = new List<Event>();
+
+        public bool ScheduleEvent(Event evt)
+        {
+            bool isDoubleBooked = this.isDoubleBooked(evt);
+            if (isDoubleBooked)
+                return false;
+
+            evtList.Add(evt);
+            return true;
+        }
+        public bool CancelEvent(Event evt)
+        {
+            Event matchedEvt = evtList.First(p => p.Name.Equals(evt.Name)
+            && p.Location.Equals(evt.Location) && p.StartTime.Equals(evt.StartTime));
+
+            if (matchedEvt == null)
+                return false;
+
+            evtList.Remove(matchedEvt);
+            return true;
+        }
+
+        public List<Event> GetUpcomingEvents(int hours = 24)
+        {
+            DateTime now = DateTime.Now;
+            DateTime till = now.AddHours(hours);
+            List<Event> list = new List<Event>();
+            foreach (var evt in evtList)
+            {
+                if (evt.StartTime <= till && evt.StartTime >= now)
+                    list.Add(evt);
+            }
+            return list;
+        }
+
+        private bool isDoubleBooked(Event newEvt)
+        {
+            if (newEvt == null || newEvt.StartTime == DateTime.MinValue) return true;
+
+            return evtList.Exists(p => p.StartTime == newEvt.StartTime);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Redox_Code_Test
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            
+            List<int> sequence = InitializeSequence(100);
+            IEnumerable<int> evenNums = GetEvenNums(sequence);
+            Console.WriteLine(string.Join(' ', evenNums));
 
+            IEnumerable<int> multiplesOf35 = GetMultiplesOf35(sequence);
+            Console.WriteLine(string.Join(' ', multiplesOf35));
+        }
+
+        public static List<int> InitializeSequence(int length)
+        {
+            List<int> sequence = new List<int>();
+            if (length < 1)
+                return sequence;
+
+            for (int i = 1; i <= length; i++)
+            {
+                sequence.Add(i);
+            }
+
+            return sequence;
+        }
+
+        public static IEnumerable<int> GetEvenNums(List<int> array)
+        {
+            if (array == null) return array;
+            return array.Where(i => i % 2 == 0);
+        }
+
+        public static IEnumerable<int> GetMultiplesOf35(List<int> array)
+        {
+            if (array == null) return array;
+            var results = from i in array
+                          where (i % 3 == 0 || i % 5 == 0) && i % 15 != 0
+                          select i;
+            return results;
         }
     }
 }
