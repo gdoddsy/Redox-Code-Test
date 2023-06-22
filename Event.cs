ec3c5510096c40cf2b4ce72d2e07e92063f581de
fritzlee21 <137294151+fritzlee21@users.noreply.github.com>
2023-06-22 10:47:48 +0000

    Code Assessment Completed

---
+++ Event.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Redox_Code_Test
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
