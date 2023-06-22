78b7e505fe09e155d2979ea824919bd755ec2b11
patrick@quadservices.com.au <patrick@quadservices.com.au>
2023-06-22 21:19:12 +1000

    Done by Patrick CHEN

---
+++ Data/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redox_Code_Test.Data
+{
+    public class Event
+    {
+        public string Id { set; get; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+        public string Location { set; get; }
+        public string DateTime { set; get; }
+    }
+}
